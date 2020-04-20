541540f409e74fea1d105314a778e2a06f77fbac
Casey Wood <cwood@mobials.com>
2020-04-20 15:07:40 -0400

    Rename and dags update
    Minor rename of folders and some files

---
+++ src/dashboard/dags/zuora_invoice_item_created_dag.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.insert(0,'../..')
+import airflow
+import datetime
+
+start_date = datetime.datetime(2000,1,1)
+
+default_args = {
+    'start_date' : start_date,
+    'retries': 0
+}
+
+dag = airflow.DAG('boot', default_args=default_args,schedule_interval=None)
+
+task = airflow.operators.BashOperator(
+    task_id='1',
+    bash_command='python /src/dasboard/etl/zuora_invoice_item_created.py',
+    dag=dag)
+
+task
