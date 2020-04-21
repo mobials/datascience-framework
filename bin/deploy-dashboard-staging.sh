e7482e684ec1b952ffb0099856e8b6d5dca98500
Jared Rietdyk <jrietdyk@mobials.com>
2020-04-21 09:48:19 -0400

    Testing mac deploy

--- bin/deploy-dashboard-staging.sh
+++ bin/deploy-dashboard-staging.sh
@@ -8,7 +8,7 @@ GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
 cd devops/ansible
 
-ansible-playbook -i inventories/dashboard/staging playbooks/deploy.yml --extra-vars "app_branch_name=$GIT_BRANCH_NAME" -vvv
+ansible-playbook -i inventories/dashboard/staging playbooks/deploy.yml --extra-vars "app_branch_name=$GIT_BRANCH_NAME ansible_python_interpreter=/usr/local/bin/python" -vvv
 
 cd ../..
 
