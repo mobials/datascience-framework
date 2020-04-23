be661d6fe495ebfa11f2966c75680a7f5671f4dc
Casey Wood <cwood@mobials.com>
2020-04-23 16:31:07 -0400

    Update execute_ddl.py

--- src/dashboard/maintenance/execute_ddl.py
+++ src/dashboard/maintenance/execute_ddl.py
@@ -1,14 +1,13 @@
 import sys
 import pathlib
-sys.path.append(str(pathlib.Path.cwd().joinpath('src')))
-import src.postgreshandler
+sys.path.append('../..')
+sys.path.append('/var/www/datascience-framework/src/')
+import postgreshandler
 import psycopg2
-
-
 
 file_path = '../ddl/relations.sql'
 
-with src.postgreshandler.get_dashboard_connection() as connection:
+with postgreshandler.get_dashboard_connection() as connection:
     with open(file_path,'r') as file:
         text = file.read().replace('\n', '')
         queries = text.split(';')
